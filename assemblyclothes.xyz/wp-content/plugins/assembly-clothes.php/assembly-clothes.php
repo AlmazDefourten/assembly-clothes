52045da541a91dcaebbfeade52ea97c0fe678465
GoshaV2 <78751429+GoshaV2@users.noreply.github.com>
2022-04-16 08:44:35 +0300

    Update assembly-clothes.php

--- assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
+++ assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
@@ -7,11 +7,38 @@
  * Author:      Имя автора
  * Version:     Версия плагина, например 1.0 
  */
-	function testShortCode($attrs){
-		return "hellow world!";
-	}
+function testShortCode($attrs){
+	return "hellow world!";
+}
 
 	add_shortcode('test','testShortCode');
-	
+	global $jal_db_version;
+$jal_db_version = "1.0";
 
+function jal_install () {
+   global $wpdb;
+   global $jal_db_version;
+
+   $table_name = $wpdb->prefix . "liveshoutbox";
+   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+      
+      $sql = "CREATE TABLE " . $table_name . " (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  time bigint(11) DEFAULT '0' NOT NULL,
+	  name tinytext NOT NULL,
+	  text text NOT NULL,
+	  url VARCHAR(55) NOT NULL,
+	  UNIQUE KEY id (id)
+	);";
+
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      dbDelta($sql);
+
+      $rows_affected = $wpdb->insert( $table_name, array( 'time' => current_time('mysql'), 'name' => $welcome_name, 'text' => $welcome_text ) );
+ 
+      add_option("jal_db_version", $jal_db_version);
+
+   }
+}
+register_activation_hook(__FILE__,'jal_install');
 ?>
