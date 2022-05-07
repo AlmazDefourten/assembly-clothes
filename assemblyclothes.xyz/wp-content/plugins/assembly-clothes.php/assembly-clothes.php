24cf0f07bcedf5013e3dcd27dc96741de39ff115
Vance12355 <haibiev.amir@gmail.com>
2022-05-07 09:47:41 +0300

    modal window

--- assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
+++ assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
@@ -27,7 +27,7 @@ function formSupplierFun($attrs){
 	global $post;
 	global $wpdb;
 
-	$sups = $wpdb->get_results("SELECT * FROM $wpdb->prefix"."cost_estimate a JOIN $wpdb->prefix"."wp_users b ON a.ID = wendorId");
+	$sups = $wpdb->get_results("SELECT * FROM $wpdb->prefix"."cost_estimate a JOIN $wpdb->prefix"."users b ON a.ID = wendorId");
 
 	?>
 	<form  action="<?= admin_url('admin-post.php'); ?>" method="post">
