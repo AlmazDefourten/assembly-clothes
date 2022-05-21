ea7efab7887dd45acc19dd61d1e158f4583c7678
Vance12355 <haibiev.amir@gmail.com>
2022-05-21 22:05:38 +0300

    furns

--- assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
+++ assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
@@ -36,7 +36,9 @@ function formFurnitureFun($attrs){
 	<input type="hidden" name="action" value="estimate_post" />
 	<input type="hidden" name="redirect" value="<?=get_page_uri()?>"/>
 	<table style="height: 346px;" width="1600">
-	<p><?=$vendors?></p>
+	<?php foreach($vendors as $vendor){
+		?><p><?=$vendor?></p><?php
+	}?>
 	<p><?=$prices?></p>
 	<tbody>
 	<tr>
