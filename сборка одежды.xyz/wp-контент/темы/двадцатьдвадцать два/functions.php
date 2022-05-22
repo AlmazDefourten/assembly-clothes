350960459b7931a0b7c9bfd86c57ba503cf4fecf
AlmazDefourten <almaz.sahabiev@outlook.com>
2022-05-22 18:13:50 +0300

    hotfix

--- сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
+++ сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
@@ -212,7 +212,7 @@ function listOfVendors() {
 	$quantity = json_decode($_REQUEST['dopPrice']);
     $dopPrice=json_decode($_REQUEST['dopPrice'], true);
 	$req = $_REQUEST['dopPrice'];
-	$req_decode = json_decode($req));
+	$req_decode = json_decode($req);
 	var_dump($req_decode);
 	var_dump($quantity);
 	var_dump($test);
