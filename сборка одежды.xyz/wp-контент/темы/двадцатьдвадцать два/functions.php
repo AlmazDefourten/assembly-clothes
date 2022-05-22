e937aa6d3c53abb9d3a38e4edeedf26488fa336c
AlmazDefourten <almaz.sahabiev@outlook.com>
2022-05-22 13:24:31 +0300

    fix

--- сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
+++ сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
@@ -298,7 +298,7 @@ function listOfVendors() {
 				$price = $wpdb->get_results("SELECT price FROM `wp_furns_price` WHERE term_id=(SELECT DISTINCT t.term_id FROM wp_posts AS p INNER JOIN wp_term_relationships AS tr ON p.id = tr.object_id INNER JOIN wp_term_taxonomy AS tt ON tt.term_taxonomy_id = tr.term_taxonomy_id INNER JOIN wp_terms AS t ON t.term_id = tt.term_id WHERE p.ID = $card_id AND tt.taxonomy = 'pa_фурнитура' AND p.post_type = 'product' AND tt.taxonomy LIKE 'pa_%' AND t.name = $furn_name)") * $furn->quan;
 				$result_conf .= "<p> $furn_name Цена: $price </p>";
 			}
-			echo($result_conf);
+			echo $result_conf;
 		}
 		if ($cut == 1) {
 			$sum+=$result->cutPrice;
