861c3ff54a5fdf75f54adc42b4d98e694db1df44
Тиунов Георгий <34783tgs@mail.ru>
2022-05-22 17:45:52 +0300

    assem

--- assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
+++ assemblyclothes.xyz/wp-content/plugins/assembly-clothes.php/assembly-clothes.php
@@ -224,7 +224,7 @@ function formEstimateFun($attrs){
 <td><input name="tailoringTime" required="" type="number" /></td>
 </tr>
 <tr>
-	<td>Дополнительные услуги</td>
+	<td colspan="2">Дополнительные услуги</td>
 </tr>
 <?
 	foreach($dop as $res){
@@ -310,7 +310,7 @@ function formEstimateFun($attrs){
 <td><input name="tailoringTime" required="" type="number" value="<?=$post_exist->tailoringTime?>"/></td>
 </tr>
 <tr>
-	<td>Дополнительные услуги</td>
+	<td colspan="2">Дополнительные услуги</td>
 </tr>
 <?
 	foreach($dop as $res){
