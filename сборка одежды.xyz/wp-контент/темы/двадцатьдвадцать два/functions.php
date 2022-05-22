750e92bd4528cf1079754f8571501a5458cf162e
AlmazDefourten <almaz.sahabiev@outlook.com>
2022-05-23 01:15:12 +0300

    sql fix

--- сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
+++ сборка одежды.xyz/wp-контент/темы/двадцатьдвадцать два/functions.php
@@ -312,7 +312,7 @@ function listOfVendors() {
 			$flag = false;
 			foreach ($furns as $furn) {
 				$furn_name = $furn['name'];
-				$price = $wpdb->get_row("SELECT a.price, b.display_name, t.name FROM `wp_furns_price` AS a JOIN `wp_users` AS b INNER JOIN wp_terms AS t ON t.term_id=a.term_id WHERE a.wendorId = b.ID AND a.term_id=(SELECT DISTINCT t.term_id FROM wp_posts AS p INNER JOIN wp_term_relationships AS tr ON p.id = tr.object_id INNER JOIN wp_term_taxonomy AS tt ON tt.term_taxonomy_id = tr.term_taxonomy_id INNER JOIN wp_terms AS t ON t.term_id = tt.term_id WHERE p.ID = $card_id AND p.post_type = 'product' AND tt.taxonomy LIKE 'pa_%' AND t.term_id = $furn_name)");
+				$price = $wpdb->get_row("SELECT a.price, b.display_name, t.name FROM `wp_furns_price` AS a JOIN `wp_users` AS b INNER JOIN wp_terms AS t ON t.term_id=a.term_id WHERE a.wendorId = b.ID AND a.wendorId='$result->wendorId' AND a.term_id=(SELECT DISTINCT t.term_id FROM wp_posts AS p INNER JOIN wp_term_relationships AS tr ON p.id = tr.object_id INNER JOIN wp_term_taxonomy AS tt ON tt.term_taxonomy_id = tr.term_taxonomy_id INNER JOIN wp_terms AS t ON t.term_id = tt.term_id WHERE p.ID = $card_id AND p.post_type = 'product' AND tt.taxonomy LIKE 'pa_%' AND t.term_id = $furn_name)");
 				$price_quan = $price->price * $furn['quan'];
 				$vendor_name = $price->display_name;
 				if ($price_quan != 0) {
